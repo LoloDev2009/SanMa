02dcabf4f1374e4452f78c87595ad662bd06e9f9
LoloDev2009 <588lorenzo@gmail.com>
2023-05-22 11:24:39 -0300

    menu desplegable

--- script.js
+++ script.js
@@ -1,29 +1,52 @@
+// Seleccionar el elemento del carrusel y las imágenes individuales
 const carouselSlide = document.querySelector('.carousel-slide');
 const slides = carouselSlide.querySelectorAll('img');
+
+// Seleccionar los botones de navegación
 const prevBtn = document.querySelector('.prev');
 const nextBtn = document.querySelector('.next');
+// Variable para mantener el índice de la diapositiva actual
 let slideIndex = 0;
 
+// Función para mostrar una diapositiva específica
 function showSlide(index) {
   slides.forEach((slide) => {
-    slide.style.display = 'none';
+    slide.style.display = 'none'; // Ocultar todas las diapositivas
   });
 
-  slides[index].style.display = 'block';
+  slides[index].style.display = 'block'; // Mostrar la diapositiva seleccionada
 }
 
+// Función para mostrar la siguiente diapositiva
 function nextSlide() {
-  slideIndex = (slideIndex + 1) % slides.length;
-  showSlide(slideIndex);
+  slideIndex = (slideIndex + 1) % slides.length; // Calcular el nuevo índice
+  showSlide(slideIndex); // Mostrar la diapositiva siguiente
 }
 
+// Función para mostrar la diapositiva anterior
 function prevSlide() {
-  slideIndex = (slideIndex - 1 + slides.length) % slides.length;
-  showSlide(slideIndex);
+  slideIndex = (slideIndex - 1 + slides.length) % slides.length; // Calcular el nuevo índice
+  showSlide(slideIndex); // Mostrar la diapositiva anterior
 }
 
+// Agregar eventos de clic a los botones de navegación
 nextBtn.addEventListener('click', nextSlide);
 prevBtn.addEventListener('click', prevSlide);
 
 // Mostrar la primera imagen inicialmente
 showSlide(slideIndex);
+
+const menuIcon = document.querySelector('.menu-icon');
+const menu = document.querySelector('.menu');
+
+menuIcon.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
+
+function toggleMenu() {
+  const menuIcon = document.querySelector('.menu-icon');
+  const menu = document.querySelector('.menu');
+  
+  menuIcon.classList.toggle('open');
+  menu.classList.toggle('open');
+}
